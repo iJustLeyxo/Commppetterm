542e493edb213d39b962c3781660d45fccf71114
NikolajSankovDev <nikolaj.sankov.dev@gmail.com>
2024-02-15 18:21:12 +0100

    calender

--- src/main/java/commppetterm/gui/page/Calender.java
+++ src/main/java/commppetterm/gui/page/Calender.java
@@ -1,6 +1,13 @@
 package commppetterm.gui.page;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 
 public class Calender implements Controller{
@@ -11,8 +18,13 @@ public class Calender implements Controller{
     }
 
     @FXML
+    private Label monthLabel;
+    @FXML
+    private GridPane calendarGrid;
+
+    @FXML
     public void initialize() {
-        // Initialisierungslogik hier
+        fillCalendar(YearMonth.now());
     }
 
     @Override
@@ -24,10 +36,26 @@ public class Calender implements Controller{
     public void postInit() {
         // Optional: Code nach dem Laden
     }
-
-    // @FXML
-    // ....
-
     
-    
+    private void fillCalendar(YearMonth yearMonth) {
+        LocalDate calendarDate = LocalDate.of(yearMonth.getYear(), yearMonth.getMonth(), 1);
+        
+        String monthName = yearMonth.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        monthLabel.setText(monthName + " " + yearMonth.getYear());
+        
+        int dayOfWeekOfFirst = calendarDate.getDayOfWeek().getValue() % 7;
+        int daysInMonth = yearMonth.lengthOfMonth();
+        
+        for (int i = 1, dayOfMonth = 1; dayOfMonth <= daysInMonth; i++) {
+            int col = (dayOfWeekOfFirst + i - 2) % 7;
+            int row = (dayOfWeekOfFirst + i - 2) / 7;
+            
+            Label dayLabel = new Label(Integer.toString(dayOfMonth));
+            dayLabel.getStyleClass().add("day-cell");
+            
+            calendarGrid.add(dayLabel, col, row);
+            dayOfMonth++;
+        }
+    }
+
 }
