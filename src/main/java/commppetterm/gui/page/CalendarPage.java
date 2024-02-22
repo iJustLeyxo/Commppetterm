99b918442e33d3077e2dabd6dac545bc841bd8eb
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-02-22 14:30:11 +0100

    Add calendar page

--- src/main/java/commppetterm/gui/page/CalendarPage.java
+++ src/main/java/commppetterm/gui/page/CalendarPage.java
@@ -1,8 +1,48 @@
 package commppetterm.gui.page;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Pane;
+
 /**
  * Calendar controller class
  */
-public final class Calendar {
-    
+public final class CalendarPage extends Controller {
+    @FXML
+    private Button editBtn, delBtn;
+
+    @FXML
+    private Label dateLab;
+
+    @FXML
+    private ToggleButton dayBtn, weekBtn, monthBtn;
+
+    @FXML
+    private Pane contentPane;
+
+    @FXML
+    private void onNew() {};
+
+    @FXML
+    private void onEdit() {}
+
+    @FXML
+    private void onDel() {};
+
+    @FXML
+    private void onPrev() {};
+
+    @FXML
+    private void onNext() {};
+
+    @FXML
+    private void onDay() {};
+
+    @FXML
+    private void onWeek() {};
+
+    @FXML
+    private void onMonth() {};
 }
