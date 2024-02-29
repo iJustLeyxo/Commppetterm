af913047e63d4099950cba40468827039bae8325
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-02-29 22:06:01 +0100

    Add logic for end and time toggleing

--- src/main/java/commppetterm/gui/page/Editor.java
+++ src/main/java/commppetterm/gui/page/Editor.java
@@ -5,7 +5,10 @@ import commppetterm.gui.exception.ControllerLoadedException;
 import commppetterm.gui.exception.FxmlLoadException;
 import commppetterm.gui.exception.URLNotFoundException;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
@@ -15,13 +18,16 @@ import javafx.scene.layout.HBox;
  */
 public final class Editor extends Controller {
     @FXML
-    private HBox startTimeHBox, endTimeHBox;
+    private HBox startTimeHBox, endHBox, endTimeHBox;
 
     @FXML
     private Button saveButton, deleteButton;
 
     @FXML
     private ToggleButton endToggleButton, timeToggleButton, recurringToggleButton;
+
+    @FXML
+    private Label endLabel;
 
     @FXML
     private TextField titleTextField, detailsTextField,
@@ -45,17 +51,21 @@ public final class Editor extends Controller {
 
     @FXML
     private void endToggleButtonAction() {
-        // TODO: Add end logic
+        this.enabled(this.endLabel, this.endToggleButton.isSelected());
+        this.enabled(this.endHBox, this.endToggleButton.isSelected());
+        Gui.get().stage().sizeToScene();
     }
 
     @FXML
     private void timeToggleButtonAction() {
-        // TODO: Add time logic
+        this.enabled(this.startTimeHBox, this.timeToggleButton.isSelected());
+        this.enabled(this.endTimeHBox, this.timeToggleButton.isSelected());
+        Gui.get().stage().sizeToScene();
     }
 
     @FXML
     private void recurringToggleButtonAction() {
-        // TODO: Add repeat logic
+        // TODO: Add recurring logic and control elements
     }
 
     @Override
@@ -63,6 +73,21 @@ public final class Editor extends Controller {
         this.endToggleButton.setSelected(false);
         this.timeToggleButton.setSelected(false);
         this.recurringToggleButton.setSelected(false);
+
+        this.enabled(this.startTimeHBox, false);
+        this.enabled(this.endTimeHBox, false);
+        this.enabled(this.endLabel, false);
+        this.enabled(this.endHBox, false);
+    }
+
+    /**
+     * Enables or disables a node, can make the node invisible and exclude the node from rendering
+     * @param node The node to edit
+     * @param value The value to set
+     */
+    private void enabled(Node node, boolean value) {
+        node.setVisible(value);
+        node.setManaged(value);
     }
 
     // TODO: Make calendar memorize date
