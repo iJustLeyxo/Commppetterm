874cd1a340ad6efaaa9e66ea121d1180a4baaeff
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-02-29 21:20:47 +0100

    Update editor finalize option buttons and fxids

--- src/main/java/commppetterm/gui/page/Editor.java
+++ src/main/java/commppetterm/gui/page/Editor.java
@@ -6,8 +6,8 @@ import commppetterm.gui.exception.FxmlLoadException;
 import commppetterm.gui.exception.URLNotFoundException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 
@@ -19,26 +19,51 @@ public final class Editor extends Controller {
     private HBox startTime, endTime;
 
     @FXML
-    private RowConstraints end;
+    private RowConstraints endRow;
 
     @FXML
     private Button save, delete;
 
     @FXML
+    private ToggleButton end, time, repeat;
+
+    @FXML
     private TextField title, detail, startDay, startMonth, startYear, startHour, startMinute, endDay, endMonth, endYear, endHour, endMinute;
 
     @FXML
-    private void save() {}
+    private void save() {
+        // TODO: Add save logic
+    }
 
     @FXML
-    private void delete() {}
+    private void delete() {
+        // TODO: Add delete logic
+    }
+
+    @FXML
+    private void end() {
+        // TODO: Add end logic
+    }
+
+    @FXML
+    private void time() {
+        // TODO: Add time logic
+    }
+
+    @FXML
+    private void repeat() {
+        // TODO: Add repeat logic
+    }
 
     @FXML
     private void cancel() throws ControllerLoadedException, URLNotFoundException, FxmlLoadException {
         Gui.get().prepare(new Calendar());
     };
 
-    // TODO: Add options for whole day events, recurring events and single day events
+    @Override
+    protected void init() {
+        // TODO: Add init logic
+    }
 
     // TODO: Make calendar memorize date
 
