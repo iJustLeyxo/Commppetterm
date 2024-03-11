d6f2935d2000d6580e397d6b84226da4515d5728
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-03-11 22:07:17 +0100

    Add database handler frame and update entry class

---
+++ src/main/java/commppetterm/database/Database.java
@@ -0,0 +1,38 @@
+package commppetterm.database;
+
+import commppetterm.entity.Entry;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Database handler
+ */
+public final class Database {
+    /**
+     * Fetches the entries of a day
+     * @param date The day to fetch the entries of
+     * @return a list of entries
+     */
+    public static List<Entry> entries(LocalDate date) {
+        // TODO: Fetch entries of date
+        return null;
+    }
+
+    /**
+     * Creates or edits and entry
+     * @param entry The entry to create or edit
+     */
+    public static void update(@NotNull Entry entry) {
+        // TODO: Update or create entry if it doesn't yet exist
+    }
+
+    /**
+     * Deletes an entry
+     * @param entry The entry to delete
+     */
+    public static void delete(@NotNull Entry entry) {
+        // TODO: Delete entry
+    }
+}
