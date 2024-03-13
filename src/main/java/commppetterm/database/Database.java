16be7342be0841d6b7bd0826742b6015edc977df
junic25 <julnicdoh@gmail.com>
2024-03-13 21:40:16 +0100

    import

--- src/main/java/commppetterm/database/Database.java
+++ src/main/java/commppetterm/database/Database.java
@@ -7,6 +7,7 @@ import commppetterm.database.Entry.Recurrence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
